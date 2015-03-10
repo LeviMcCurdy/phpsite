38fa9ecc1ba3000a3854b729ef719c753222eb66
Levi McCurdy <levi@levimccurdy.com>
2015-03-10 10:59:05 -0400

    First Commit
    Added hello world to the index.php

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+// Define variables for SEO
+$pageTitle = 'put page title here'; // Title of page //
+$pageDescription = 'put page description here'; // Title of page //
+$pageCanonical = 'http://www.url.com/';
+// If we don't want the search engines to see our website just yet
+//$pageRobots = 'noindex,nofollow';
+include('inc/header.php') // Include header file //
+?>
+
+
+
+        HELLO WORLD
+        
+
+	
+ 
+<?php include('inc/footer.php'); ?>
